dfd878a26d5814123265011fd011490b01f88fb4
Shyam Grover <post2shyam@gmail.com>
2020-09-17 18:37:02 +0530

    Refactored code in Service class and invoked using Dependency Injection support under Spring.

--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -1,27 +1,23 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import technicalblog.model.Post;
+import technicalblog.service.PostService;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Controller
 public class HomeController {
+
+  @Autowired
+  private PostService postService;
+
   @RequestMapping("/")
   public String getAllPosts(Model model) {
-    Post post1 = new Post("post 1", "Post body 1", new Date());
-    Post post2 = new Post("post 2", "Post body 2", new Date());
-    Post post3 = new Post("post 3", "Post body 3", new Date());
-
-    List<Post> postCollection = new ArrayList<>();
-    postCollection.add(post1);
-    postCollection.add(post2);
-    postCollection.add(post3);
-
+    List<Post> postCollection  = postService.getAllPosts();
     model.addAttribute("posts", postCollection);
 
     return "index";
---
+++ src/main/java/technicalblog/service/PostService.java
@@ -0,0 +1,24 @@
+package technicalblog.service;
+
+import org.springframework.stereotype.Service;
+import technicalblog.model.Post;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class PostService {
+  public List<Post> getAllPosts() {
+    Post post1 = new Post("post 1", "Post body 1", new Date());
+    Post post2 = new Post("post 2", "Post body 2", new Date());
+    Post post3 = new Post("post 3", "Post body 3", new Date());
+
+    List<Post> postCollection = new ArrayList<>();
+    postCollection.add(post1);
+    postCollection.add(post2);
+    postCollection.add(post3);
+
+    return postCollection;
+  }
+}
