ec0371bbeaf267166783245d391770346f1b90f4
Shyam Grover <post2shyam@gmail.com>
2020-10-08 15:13:43 +0530

    Post model class is now connected to PostGreSql using JPA, queries are dont using JPL

--- src/main/java/technicalblog/model/Post.java
+++ src/main/java/technicalblog/model/Post.java
@@ -1,17 +1,33 @@
 package technicalblog.model;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "posts")
 public class Post {
 
+  @Id
+  @Column(name = "id")
+  private Integer id;
+
+  @Column(name = "title")
   private String title;
+
+  @Column(name = "body")
   private String body;
+
+  @Transient
   private Date date;
 
   public Post(String title, String body, Date date) {
     this.title = title;
     this.body = body;
     this.date = date;
+  }
+
+  public Post() {
+    
   }
 
   public String getTitle() {
--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -3,6 +3,10 @@ package technicalblog.service;
 import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.TypedQuery;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,29 +16,35 @@ public class PostService {
 
   private static String dpPassword = "1234";
 
-  public List<Post> getAllPosts() {
-    List<Post> posts = new ArrayList<>();
-    Connection connection = null;
-    try {
-      connection = getDbConnection();
-      Statement statement = connection.createStatement();
-      ResultSet rs = statement.executeQuery("SELECT * FROM posts");
+  @PersistenceUnit(unitName = "techblog")
+  private EntityManagerFactory emf;
 
-      // Iterate through the posts of the db
-      while (rs.next()) {
-        String title = rs.getString("title");
-        String body = rs.getString("body");
-        posts.add(new Post(title, body, null));
-      }
-    } catch (ClassNotFoundException | SQLException exception) {
-      exception.printStackTrace();
-    } finally{
-      try {
-        connection.close();
-      } catch (SQLException throwables) {
-        throwables.printStackTrace();
-      }
-    }
+  public List<Post> getAllPosts() {
+    //JPQL query
+    TypedQuery<Post> query = getEntityManager().createQuery("SELECT p from Post p", Post.class);
+    List<Post> posts = query.getResultList();
+
+//    Connection connection = null;
+//    try {
+//      connection = getDbConnection();
+//      Statement statement = connection.createStatement();
+//      ResultSet rs = statement.executeQuery("SELECT * FROM posts");
+//
+//      // Iterate through the posts of the db
+//      while (rs.next()) {
+//        String title = rs.getString("title");
+//        String body = rs.getString("body");
+//        posts.add(new Post(title, body, null));
+//      }
+//    } catch (ClassNotFoundException | SQLException exception) {
+//      exception.printStackTrace();
+//    } finally {
+//      try {
+//        connection.close();
+//      } catch (SQLException throwables) {
+//        throwables.printStackTrace();
+//      }
+//    }
     return posts;
   }
 
@@ -64,6 +74,10 @@ public class PostService {
 
   public void createPost(Post newPost) {
     // TODO
+  }
+
+  private EntityManager getEntityManager() {
+    return emf.createEntityManager();
   }
 
   private Connection getDbConnection() throws ClassNotFoundException, SQLException {
